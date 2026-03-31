a318925b5f0865be0af0a972c952c3f6baf17d12
albamarialid <allid1228@uib.no>
2026-03-31 12:13:35 +0200

    Update script.js
    Henter ut selve cocktail objektet fra responsen

--- script.js
+++ script.js
@@ -125,7 +125,9 @@ Returns a Promise that resolves to cocktail object
 function fetchRandomCocktail() {
   return fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   .then((response) => response.json())
-
+  .then((data) => {
+    return data.drinks[0];
+  });
 }
 
 /*
